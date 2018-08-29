09d5ccfa52a3a0037a66b072fba0153a07a25e7d
socialflow <john.m.lenehan@gmail.com>
2018-08-29 03:08:02 -0400

    all passing callback via query param

--- server/app/routes/deeplinking/index.js
+++ server/app/routes/deeplinking/index.js
@@ -2,6 +2,8 @@
 const router = require('express').Router(); // eslint-disable-line new-cap
 const path = require('path');
 const articles = require('../../../static/articles.js');
+const url = require('url');
+
 module.exports = router;
 
 router.get('/', function (req, res) { 
@@ -10,7 +12,9 @@ router.get('/', function (req, res) {
 
 //router.use(function(req,res,next){setTimeout(next,10000)});
 router.get('/:article', function (req, res) { 
-    res.render('articles/article',{layout: 'deeplinking'});
+    var { callback_url } = req.query;
+    var sanitized =  callback_url ? req.sanitize(callback_url) : fullUrl(req);
+    res.render('articles/article',{layout: 'deeplinking',callback: sanitized});
 });
 // Make sure this is after all of
 // the registered routes!
@@ -19,3 +23,11 @@ router.use(function (req, res, next) {
     err.status = 404;
     next(err);
 });
+
+function fullUrl(req) {
+  return url.format({
+    protocol: req.protocol,
+    host: req.get('host'),
+    pathname: req.originalUrl
+  });
+}
