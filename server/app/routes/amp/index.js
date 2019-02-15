259d865cf6d479383c6e987ffa752d7d2619adf4
socialflow <john.m.lenehan@gmail.com>
2019-02-15 15:34:18 -0500

    derp

--- server/app/routes/amp/index.js
+++ server/app/routes/amp/index.js
@@ -18,7 +18,8 @@ router.get('/:article', function (req, res) {
 router.get('/:article/amplink', function (req, res) { 
     var name = req.sanitize(req.params.article);
     let url = req.protocol + '://'+req.get('host') + req.originalUrl;
-    console.log("FULL URL: "+req.protocol + '://'+req.get('host') + req.originalUrl)
+    if(url[url.length-1] !=='/') url+='/';
+    console.log("FULL URL: "+url)
     res.render('articles/article',{...articles['article1'],article_name: name, full_url: url,layout: 'amp',isAmp: true});
 });
 // Make sure this is after all of
