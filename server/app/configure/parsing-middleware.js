9aa1eeb3fd0417e8b08fd63c873772118818d2f9
socialflow <john.m.lenehan@gmail.com>
2019-02-27 13:34:54 -0500

    log useragent

--- server/app/configure/parsing-middleware.js
+++ server/app/configure/parsing-middleware.js
@@ -1,5 +1,6 @@
 'use strict';
 var cookieParser = require('cookie-parser');
+const useragent = require('express-useragent');
 var bodyParser = require('body-parser');
 var expressSanitizer = require('express-sanitizer');
 
@@ -7,5 +8,6 @@ module.exports = function (app) {
     app.use(cookieParser());
     app.use(bodyParser.json());
     app.use(expressSanitizer());
+    app.use(useragent.express());
     app.use(bodyParser.urlencoded({ extended: true }));
 };
