458d74ec8fb39e2735b3804a4eeb9c6aaf15387b
socialflow <john.m.lenehan@gmail.com>
2019-03-05 17:04:50 -0500

    add ip logging (oh noes, sorry GDPR)

--- server/app/configure/app-variables.js
+++ server/app/configure/app-variables.js
@@ -11,10 +11,15 @@ var env = require(path.join(rootPath, './server/env'));
 
 var logMiddleware = function (req, res, next) {
     util.log(('---NEW REQUEST---'));
+    let  ip = (req.headers['x-forwarded-for'] || '').split(',').pop() || 
+        req.connection.remoteAddress || 
+        req.socket.remoteAddress || 
+        req.connection.socket.remoteAddress;
     console.log(util.format(chalk.red('%s: %s %s'), 'REQUEST ', req.method, req.path));
     console.log(util.format(chalk.yellow('%s: %s'), 'QUERY   ', util.inspect(req.query)));
     console.log(util.format(chalk.cyan('%s: %s'), 'BODY    ', util.inspect(req.body)));
     console.log(util.format(chalk.blue('%s: %s'), 'UA    ', util.inspect(req.useragent.source)));
+    console.log(util.format(chalk.magenta('%s: %s'), 'IP    ', ip));
     next();
 };
 
